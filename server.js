39ff79ff167ea3c76598319bd37bb5ba25afb20c
Huzaifa Asif <huzaifa@theaweb.net>
2021-07-24 00:57:04 +0500

    deployment error fix

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist'));
+app.use(express.static('./dist/Frontend-Martian-Robots'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/Frontend-Martian-Robots/'}),
+    res.sendFile('index.html', {root: 'dist/Frontend-Martian-Robots'}),
 );
 
 app.listen(process.env.PORT || 8080);
